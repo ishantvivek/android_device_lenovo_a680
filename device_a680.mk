27ade2730b02c43b5e7f8a168d342af7f2e9e79e
ishantvivek <ishantvivek1311@gmail.com>
2015-11-09 19:31:52 +0530

    Update modules

--- device_a680.mk
+++ device_a680.mk
@@ -16,19 +16,18 @@ PRODUCT_COPY_FILES += \
 	$(MOD_SRC)/devapc.ko:$(MOD_TGT)/devapc.ko \
 	$(MOD_SRC)/devinfo.ko:$(MOD_TGT)/devinfo.ko \
 	$(MOD_SRC)/mtk_fm_drv.ko:$(MOD_TGT)/mtk_fm_drv.ko \
-	$(MOD_SRC)/mtk_hif_sdio.ko:$(MOD_TGT)/mtk_hif_sdio.ko \
+	$(MOD_SRC)/mali.ko:$(MOD_TGT)/mali.ko \
 	$(MOD_SRC)/mtk_stp_bt.ko:$(MOD_TGT)/mtk_stp_bt.ko \
 	$(MOD_SRC)/mtk_stp_gps.ko:$(MOD_TGT)/mtk_stp_gps.ko \
 	$(MOD_SRC)/mtk_stp_uart.ko:$(MOD_TGT)/mtk_stp_uart.ko \
 	$(MOD_SRC)/mtk_stp_wmt.ko:$(MOD_TGT)/mtk_stp_wmt.ko \
 	$(MOD_SRC)/mtk_wmt_wifi.ko:$(MOD_TGT)/mtk_wmt_wifi.ko \
-	$(MOD_SRC)/mtklfb.ko:$(MOD_TGT)/mtklfb.ko \
-	$(MOD_SRC)/pvrsrvkm.ko:$(MOD_TGT)/pvrsrvkm.ko \
+	$(MOD_SRC)/hid-logitech.ko:$(MOD_TGT)/hid-logitech.ko \
 	$(MOD_SRC)/scsi_tgt.ko:$(MOD_TGT)/scsi_tgt.ko \
 	$(MOD_SRC)/scsi_wait_scan.ko:$(MOD_TGT)/scsi_wait_scan.ko \
 	$(MOD_SRC)/sec.ko:$(MOD_TGT)/sec.ko \
 	$(MOD_SRC)/vcodec_kernel_driver.ko:$(MOD_TGT)/vcodec_kernel_driver.ko \
-	$(MOD_SRC)/wlan_mt6628.ko:$(MOD_TGT)/wlan_mt6628.ko
+	$(MOD_SRC)/wlan_mt.ko:$(MOD_TGT)/wlan_mt.ko
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += device/lenovo/a680/overlay
